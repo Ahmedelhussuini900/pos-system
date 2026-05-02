9bb73d67a1e60b6ce2f318332305647c183bdc92
AhmedElhussuini1200 <ahmed.elhussini@webstdy.com>
2026-05-02 19:48:10 +0300

    Initial commit

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        $this->autoBindRepositories();
+    }
+
+    /**
+     * Auto-bind all Repository Interfaces to their implementations.
+     */
+    private function autoBindRepositories(): void
+    {
+        $interfacesPath = app_path('Repositories/Interfaces');
+
+        if (! is_dir($interfacesPath)) {
+            return;
+        }
+
+        foreach (scandir($interfacesPath) as $file) {
+            if (! str_ends_with($file, 'Interface.php')) {
+                continue;
+            }
+
+            $interfaceClass = 'App\\Repositories\\Interfaces\\' . str_replace('.php', '', $file);
+            $concreteClass = 'App\\Repositories\\' . str_replace(['Interface.php'], ['.php'], $file);
+            $concreteClass = str_replace('.php', '', $concreteClass);
+
+            if (class_exists($concreteClass)) {
+                $this->app->bind($interfaceClass, $concreteClass);
+            }
+        }
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
